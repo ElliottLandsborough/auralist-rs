9ad781d03bf89bec7ba68a529646c89ad045fb35
Elliott Landsborough <elliott.landsborough@gmail.com>
2021-02-03 02:02:54 +0000

    First part of compression

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use walkdir::WalkDir;
 use std::path::Path;
 use std::time::Duration;
 use ini::Ini;
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::bufread::GzEncoder;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Result as ioResult;
 
 #[derive(Debug)]
-struct File {
+struct IndexedFile {
     id: i32,
     path: String,
 }
@@ -69,7 +75,13 @@ fn index(conn: Connection) {
 
     test_db(&conn);
 
-    backup_db(&conn, "./auralist.sqlite3", db_backup_progress);
+    backup_db_to_file(&conn, "./auralist.sqlite3", db_backup_progress);
+
+    compress_file("./auralist.sqlite3");
+
+    //decompress_file("./auralist.sqlite3");
+
+    //restore_db();
 }
 
 fn db_backup_progress(progress: backup::Progress) {
@@ -105,7 +117,7 @@ fn get_files(directory: std::string::String, conn: &Connection) -> Result<i32, w
 }
 
 fn save_file_in_db(path: std::string::String, conn: &Connection) -> SQLiteResult<()> {    
-    let f = File {
+    let f = IndexedFile {
         id: 0,
         path: path,
     };
@@ -121,7 +133,7 @@ fn save_file_in_db(path: std::string::String, conn: &Connection) -> SQLiteResult
 fn test_db(conn: &Connection) -> SQLiteResult<()> {
     let mut stmt = conn.prepare("SELECT id, path FROM file")?;
     let file_iter = stmt.query_map(params![], |row| {
-        Ok(File {
+        Ok(IndexedFile {
             id: row.get(0)?,
             path: row.get(1)?,
         })
@@ -136,7 +148,7 @@ fn test_db(conn: &Connection) -> SQLiteResult<()> {
     Ok(())
 }
 
-fn backup_db<P: AsRef<Path>>(
+fn backup_db_to_file<P: AsRef<Path>>(
     src: &Connection,
     dst: P,
     progress: fn(backup::Progress),
@@ -144,4 +156,20 @@ fn backup_db<P: AsRef<Path>>(
     let mut dst = Connection::open(dst)?;
     let backup = backup::Backup::new(src, &mut dst)?;
     backup.run_to_completion(5, Duration::from_millis(250), Some(progress))
+}
+
+fn compress_file<P: AsRef<Path>>(path: P) /*-> ioResult<Vec<u8>>*/ {
+    let f = File::open(path);
+    let b = BufReader::new(f.unwrap());
+    let gz = GzEncoder::new(b, Compression::fast());
+    //let mut buffer = Vec::new();
+    //gz.read_to_end(&mut buffer)?;
+    //Ok(buffer)
+
+    for byte in gz.bytes() {
+        println!("{}", byte.unwrap());
+        // todo: output every 50kb to file
+        // todo: decompress file
+        // todo: restore db from file
+    }
 }
