a22aaa8570549b96103eac412bc149426397d524
Elliott Landsborough <elliott.landsborough@gmail.com>
2021-06-25 10:49:39 +0100

    State example

--- src-js/HelloWorld.js
+++ src-js/HelloWorld.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Howl, Howler} from 'howler';
 
-const HelloWorld = () => {
-  return (
-      <h3>Hello World</h3>
-  );
-};
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 
 export default HelloWorld;
