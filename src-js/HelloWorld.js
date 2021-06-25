643d5aba731476eea3d506b1ada37af295e3b19b
Elliott Landsborough <elliott.landsborough@gmail.com>
2021-06-25 10:52:50 +0100

    Button example

--- src-js/HelloWorld.js
+++ src-js/HelloWorld.js
@@ -9,11 +9,24 @@ class HelloWorld extends React.Component {
     };
   }
 
+  saySomething(something) {
+    console.log(something);
+  }
+
+  handleRandomClick(e) {
+    this.saySomething("element clicked");
+  }
+
+  componentDidMount() {
+    this.saySomething("component did mount");
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={this.handleRandomClick.bind(this)}>Random Track</button>
       </div>
     );
   }
