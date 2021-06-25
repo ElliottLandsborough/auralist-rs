a0a67d0a0e8418adb8d41867263695f7c0e65287
Elliott Landsborough <elliott.landsborough@gmail.com>
2021-06-25 11:19:30 +0100

    Bring old functions in

--- src-js/HelloWorld.js
+++ src-js/HelloWorld.js
@@ -21,6 +21,43 @@ class HelloWorld extends React.Component {
     this.saySomething("component did mount");
   }
 
+  getUrl(path) {
+    let domainPrefix = 'http://localhost:1337/';
+
+    if (window.location.hostname === 'randomsound.uk') {
+        domainPrefix = 'https://randomsound.uk/';
+    }
+
+    return domainPrefix + path;
+  }
+
+  playRandomTune() {
+    var request = new XMLHttpRequest();
+    request.open('GET', getUrl('random'), true);
+    request.onload = function() {
+      if (this.status == 200) {
+        let resp = this.response;
+        let obj = JSON.parse(resp); 
+        let title = obj.data[0].title;
+        let artist = obj.data[0].artist;
+        let album = obj.data[0].album;
+        let file_name = obj.data[0].file_name;
+        let path = obj.data[0].path.replace('/home/ubuntu/music-sorted/', 'https://randomsound.uk/files/');
+        let audio = document.getElementById('audio');
+        let source = document.getElementById('audioSource');
+        source.src = path;
+        audio.load();
+        audio.play();
+        document.querySelector('#title').innerHTML = title;
+        document.querySelector('#artist').innerHTML = artist;
+        document.querySelector('#album').innerHTML = album;
+        document.querySelector('#file').innerHTML = file_name;
+      }
+    }
+
+    request.send();
+  }
+
   render() {
     return (
       <div>
