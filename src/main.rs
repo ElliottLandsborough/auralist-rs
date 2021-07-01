532f384ee5b7023b5c293536ad0c03295d0f47ce
Elliott Landsborough <elliott.landsborough@gmail.com>
2021-07-01 02:34:39 +0100

    Less pointless output

--- src/main.rs
+++ src/main.rs
@@ -368,15 +368,17 @@ async fn serve() {
 
 async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply, Infallible> {
     let (code, message) = if err.is_not_found() {
-        eprintln!("Unhandled error 1: {:?}", err);
         (
             StatusCode::NOT_FOUND,
-            "Not Found".to_string(),
+            "Not Found".to_string()
         )
     } else if err.find::<warp::reject::PayloadTooLarge>().is_some() {
-        (StatusCode::BAD_REQUEST, "Payload too large".to_string())
+        (
+            StatusCode::BAD_REQUEST,
+            "Payload too large".to_string()
+        )
     } else {
-        eprintln!("unhandled error 2: {:?}", err);
+        eprintln!("unhandled error: {:?}", err);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
             "Internal Server Error".to_string(),
